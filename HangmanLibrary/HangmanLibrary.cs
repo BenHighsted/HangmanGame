ac338daaea0c84ecdda6e8876923a7918fae3b24
Ben Highsted <benhighsted@Bens-MacBook-Pro.local>
2020-03-16 10:40:48 +1300

    Created the basic library; currently no GUI or unit tests.

---
+++ HangmanLibrary/HangmanLibrary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HangmanLibrary
+{
+    public class Hangman
+    {
+        private readonly String[] words = { "Hello World", "Developer", "C Sharp", "Coder" };
+
+        /// <summary>
+        /// Generates a random word that the user will have to try and guess.
+        /// This should be called during initiation of the GUI.
+        /// </summary>
+        /// <returns>Returns the generated word.</returns>
+        public String GenerateWord()
+        {
+            Random rand = new Random();
+            return words[rand.Next(words.Length)];
+        }
+
+        /// <summary>
+        /// Checks if the letter guessed is contained in the answer.
+        /// </summary>
+        /// <param name="word">The word the user is trying to guess</param>
+        /// <param name="letter">The letter the user has guessed</param>
+        /// <returns>If the letter is contained, the index of the letter is returned</returns>
+        public int CheckLetter(String word, String letter)
+        {
+            String[] letters = word.Split("");
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i].Equals(letter))
+                    return i;
+            }
+            return -1; //if not contained in word, return -1 to indicate it wasnt found
+        }
+
+        /// <summary>
+        /// Checks if the word guessed matches the answer.
+        /// </summary>
+        /// <param name="word">The word the user is trying to guess</param>
+        /// <param name="guess">The word the user has entered as their guess</param>
+        /// <returns>Returns true if the guess was correct, otherwise it returns else</returns>
+        public bool GuessWord(String word, String guess)
+        {
+            if (word.Equals(guess))
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
